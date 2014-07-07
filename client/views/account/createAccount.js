ebad25e86b5abeec0e2e116bfc94bcbf4e177959
AnotherJoSmith <jsmit205@uottawa.ca>
2014-07-07 12:49:11 -0400

    Adding create account backend

---
+++ client/views/account/createAccount.js
@@ -0,0 +1,20 @@
+Template.createAccount.events({
+  "submit form": function(evt){
+    evt.preventDefault();
+    var password = $(evt.target).find("[name=password]").val();
+    var confirmPassword = $(evt.target).find("[name=confirmPassword]").val();
+    if(validatePassword(password, confirmPassword)){
+      Accounts.createUser({
+        email: $(evt.target).find("[name=email]").val(),
+        password: password
+      }, Router.go("/feeds"));
+    }
+    else{
+      console.log("Passwords don't match or password not valid");
+    }
+  }
+})
+
+function validatePassword(password, confirmPassword){
+  return (password.length >= 8 && password === confirmPassword);
+}
