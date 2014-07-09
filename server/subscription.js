61bb356c7987e2db5a7dac28e88b57589a54efb9
AnotherJoSmith <jsmit205@uottawa.ca>
2014-07-09 12:01:14 -0400

    Adding subscriptions

---
+++ server/subscription.js
@@ -0,0 +1,25 @@
+// Publish the user's feeds.
+Meteor.publish("feeds", function(){
+	return Feeds.find({userId: this.userId});
+});
+
+// Publish the user's articles for this feed.
+Meteor.publish("articles", function(feedId){
+	return Articles.find({feedId: feedId, userId: this.userId});
+});
+
+
+Articles.allow({
+	update: function(userId, doc, fieldNames, modifier ){
+		return doc.userId == userId;
+	}
+});
+
+Feeds.allow({
+	update: function(userId, doc, fieldNames, modifier){
+		return doc.userId == userId;
+	},
+	remove: function(userId, doc){
+		return doc.userId == userId;
+	}
+});
