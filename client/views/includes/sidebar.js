15964f542e59cdcc524deda7c9ad8e7106360bea
nicolas <nicolas@127.0.0.1>
2014-07-10 15:44:12 -0400

    Folders can now be selected via the UI. Folder icons will change accordingly (open/close).

--- client/views/includes/sidebar.js
+++ client/views/includes/sidebar.js
@@ -10,6 +10,23 @@ Template.sidebar.events({
   }
 });
 
-function changeFolder(folder){
-    console.log("This doesn't work");
-}
+var folderOpen = "glyphicon glyph-pad glyphicon-folder-open";
+var folderClose = "glyphicon glyph-pad glyphicon-folder-close";
+
+Meteor.methods({
+    changeFolder: function(folder) {
+        var active = document.getElementById(folder);
+        var old =  document.getElementsByClassName("selected")[0];
+
+        if(old.id != "Inbox" && old.id != "Shared"){
+            old.firstChild.className = folderClose;
+        }
+
+        if(folder != "Inbox" && folder != "Shared"){
+            active.firstChild.className = folderOpen;
+        }
+
+        document.getElementsByClassName("selected")[0].className = "";
+        active.className = "bg-primary selected";
+    }
+});
