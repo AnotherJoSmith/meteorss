5663ad523d334ea086247c7ecaa061b748da38b8
AnotherJoSmith <jsmit205@uottawa.ca>
2014-07-10 14:24:34 -0400

    Adding sync button functionality

--- client/views/includes/sidebar.js
+++ client/views/includes/sidebar.js
@@ -1,4 +1,12 @@
-
+Template.sidebar.events({
+  "click #sync-feeds": function(evt){
+    Meteor.call("refreshFeeds", function(error){
+      if(error){
+        console.log(error);
+      }
+    });
+  }
+});
 
 function changeFolder(folder){
     console.log("This doesn't work");
