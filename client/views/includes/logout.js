9a5777917056f9fea520484fac749e984407c24f
AnotherJoSmith <jsmit205@uottawa.ca>
2014-07-18 23:18:47 -0400

    Adding sticky logout

--- client/views/includes/logout.js
+++ client/views/includes/logout.js
@@ -5,6 +5,16 @@ Template.logout.events({
   }
 });
 
+Template.logout.helpers({
+  userEmail: function(){
+    return Meteor.user().emails[0].address;
+  }
+})
+
+Template.logout.rendered = function(){
+  $("#logout").sticky({topSpacing: 0});
+}
+
 function logoutCallback(error){
   if(error){
     console.log("Error logging out.");
