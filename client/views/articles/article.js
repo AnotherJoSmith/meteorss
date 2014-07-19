b34aae3b57fb8347b6b515b59d4641bcd34949ae
AnotherJoSmith <jsmit205@uottawa.ca>
2014-07-19 11:01:47 -0400

    Adding domain to articles

---
+++ client/views/articles/article.js
@@ -0,0 +1,12 @@
+Template.article.helpers({
+  domain: function(){
+    var a = document.createElement('a');
+    if(this.origlink){
+      a.href = this.origlink;
+    }
+		else {
+      a.href = this.link;
+    }
+		return a.hostname;
+  }
+});
